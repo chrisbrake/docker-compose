f6214c9d9aa2ae4ae30584ad4a8257fbe5df0615
chrisbrake <chris.brake@gmail.com>
2018-08-24 23:03:50 -0600

    cleaned up quotes

--- web/letsencrypt/staging.sh
+++ web/letsencrypt/staging.sh
@@ -4,7 +4,7 @@ docker run -it --rm \
 -v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
 -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
 -v /docker/letsencrypt-docker-nginx/src/letsencrypt/site:/data/letsencrypt \
--v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
+-v /docker-volumes/var/log/letsencrypt:/var/log/letsencrypt \
 certbot/certbot \
 certonly --webroot \
 --register-unsafely-without-email --agree-tos \
