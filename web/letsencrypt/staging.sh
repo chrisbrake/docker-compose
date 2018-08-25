06b586d1503b9d239392fdbbe24a1802eca90514
chrisbrake <chris.brake@gmail.com>
2018-08-24 22:58:53 -0600

    fixed path

--- web/letsencrypt/staging.sh
+++ web/letsencrypt/staging.sh
@@ -3,7 +3,7 @@
 docker run -it --rm \
 -v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
 -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
--v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
+-v /docker/letsencrypt-docker-nginx/src/letsencrypt/site:/data/letsencrypt \
 -v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
 certbot/certbot \
 certonly --webroot \
