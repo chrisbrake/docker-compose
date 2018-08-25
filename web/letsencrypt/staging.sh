b149db55cc5b866391abbb57fe8fd397b83a327f
chrisbrake <chris.brake@gmail.com>
2018-08-24 23:10:21 -0600

    cleaned up volumes

--- web/letsencrypt/staging.sh
+++ web/letsencrypt/staging.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -it --rm \
--v /docker-volumes/etc/letsencrypt:/etc/letsencrypt:z \
--v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt:z \
--v /docker/letsencrypt-docker-nginx/src/letsencrypt/site:/data/letsencrypt:z \
--v /docker-volumes/var/log/letsencrypt:/var/log/letsencrypt:z \
+-v ./site:/data/letsencrypt:z \
 certbot/certbot \
 certonly --webroot \
 --register-unsafely-without-email --agree-tos \
