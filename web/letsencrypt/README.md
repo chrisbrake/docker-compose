36e714bc3a515d058281c31987bf8d3c722182ef
chrisbrake <chris.brake@gmail.com>
2018-08-24 23:27:29 -0600

    Added prod request

--- web/letsencrypt/README.md
+++ web/letsencrypt/README.md
@@ -1,6 +1,8 @@
 #Let's Encrypt Bootstrap
 
     docker-compose up -d
-    ./staging.sh
+    ./staging.sh # Verify you have no errors here.
+    ./requestCert.sh youremail@domain.com
+    
 
 Based on [this article](https://www.humankode.com/ssl/how-to-set-up-free-ssl-certificates-from-lets-encrypt-using-docker-and-nginx).
---
+++ web/letsencrypt/requestCert.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker run -it --rm \
+-v "/$(pwd)/tmp/etc/letsencrypt:/etc/letsencrypt:z" \
+-v "/$(pwd)/tmp/var/lib/letsencrypt:/var/lib/letsencrypt:z" \
+-v "/$(pwd)/site:/data/letsencrypt:z" \
+-v "/$(pwd)/tmp/var/log/letsencrypt:/var/log/letsencrypt:z" \
+certbot/certbot \
+certonly --webroot \
+--email "{$1}" --agree-tos --no-eff-email \
+--webroot-path=/data/letsencrypt \
+--config-dir ~/.certbot/config \
+--logs-dir ~/.certbot/logs \
+--work-dir ~/.certbot/work \
+-d muz.ca -d www.muz.ca
