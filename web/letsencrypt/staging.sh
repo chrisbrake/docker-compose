d7114d305946ef4cbbad25298b10b781cb395a4e
chrisbrake <chris.brake@gmail.com>
2018-08-25 22:35:53 -0600

    Updated staging to use a volume instead of local filesystem mount

--- web/letsencrypt/staging.sh
+++ web/letsencrypt/staging.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
+docker volume create --name letsencrypt
+
 docker run -it --rm \
--v "/$(pwd)/tmp/etc/letsencrypt:/etc/letsencrypt:z" \
--v "/$(pwd)/tmp/var/lib/letsencrypt:/var/lib/letsencrypt:z" \
+-v letsencrypt:/etc/letsencrypt \
 -v "/$(pwd)/site:/data/letsencrypt:z" \
--v "/$(pwd)/tmp/var/log/letsencrypt:/var/log/letsencrypt:z" \
 certbot/certbot \
 certonly --webroot \
 --register-unsafely-without-email --agree-tos \
 --webroot-path=/data/letsencrypt \
 --staging \
---config-dir ~/.certbot/config \
---logs-dir ~/.certbot/logs \
---work-dir ~/.certbot/work \
 -d muz.ca -d www.muz.ca
