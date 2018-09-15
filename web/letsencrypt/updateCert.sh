f727684cc721b30372305e25d07a1dd8456a9e6b
chrisbrake <chris.brake@gmail.com>
2018-09-14 21:57:16 -0600

    First draft of a script to update certs

---
+++ web/letsencrypt/updateCert.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker run --rm -it --name certbot \
+    -v letsencrypt:/etc/letsencrypt \
+    -v /docker-volumes/data/letsencrypt:/data/letsencrypt \
+    certbot/certbot renew \
+    --webroot -w /data/letsencrypt --quiet \
+    \
+    && docker kill --signal=HUP production-nginx-container
